0b1359da6950e8f38a7800789ae6e0b0854e9ef4
kamephis <kamephis@gmail.com>
2022-12-28 18:17:39 +0100

    Update widget.js
    adding the roles directly to the master.html makes this obsolete.

--- FAQ/Custom/view/base/web/js/content-type/faq/appearance/default/widget.js
+++ FAQ/Custom/view/base/web/js/content-type/faq/appearance/default/widget.js
@@ -6,10 +6,7 @@ define([
 
     return function (config, element) {
         var element = $(element);
-        element.children("li").each(function (i) {
-            $(this).find("[data-collapsible]").attr("data-role", "collapsible");
-            $(this).find("[data-content]").attr("data-role", "content");
-        });
+
         element.accordion({
             collapsible: true,
             multipleCollapsible: true
